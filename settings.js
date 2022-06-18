a3de4889bbaa8659b6d272b90633dc58b4401a5a
RanggaBotzz <107257184+RanggaBotzz@users.noreply.github.com>
2022-06-19 06:53:14 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,7 +22,7 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6285850539404','6285850539404','6285850539404'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6285779150501','6285850539404','6285850539404'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
 global.premium = ['6285850539404'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
 global.ownernomer = '6285850539404' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
